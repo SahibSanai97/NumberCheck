4c64772adc498a2d6e237aafc3c3b0b122c0a1cc
sahib.sanai <sahib.sanai@rabita.az>
2022-05-04 11:36:37 +0400

    feature(git project created):

---
+++ ChechNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿int number = int.Parse(Console.ReadLine());
+int temp = number;
+int i = 10;
+
+while (true)
+{
+    if (number / 100 == 0)
+    {
+        if (temp / i == temp % 10 || temp / 10 == 0)
+        {
+            if (temp / 100 == 0)
+            {
+                Console.WriteLine("is true");
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("is not true");
+            break;
+        }
+
+        temp = temp % i / 10;
+        i = i / 100;
+    }
+    else
+    {
+        number = number / 10;
+        i = i * 10;
+    }
+}
+
+Console.ReadKey();
